9f3b5f54bb45e9f6c156227980eef8a391f262c7
Alvin <alvinramoutar@gmail.com>
2017-05-17 12:06:53 -0400

    Push of test LibGDX environment

---
+++ desktop/src/com/playingwithlibgdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.playingwithlibgdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.playingwithlibgdx.PlayingWithLibGDX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlayingWithLibGDX(), config);
+	}
+}
